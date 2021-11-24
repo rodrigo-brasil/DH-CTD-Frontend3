d982773eb8820592754af7935beb917d9ce26c83
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-11-24 12:12:39 -0300

    aula 19 rotas

--- aula14-api/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const viacepApi = axios.create({
-    baseURL: "https://viacep.com.br/ws/"
-});
-
-export const brasilApi = axios.create({
-    baseURL: "https://brasilapi.com.br/api/cep/v2/"
-});
